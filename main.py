54ca9902cf4e099839803acfc72c5ae94743ef0e
ESX <evgeniy_h@list.ru>
2023-08-01 00:02:46 +1000

    task #2

--- main.py
+++ main.py
@@ -23,4 +23,25 @@ with open('recipes.txt', encoding='utf-8') as f:
             name, qt, msure = line.split(" | ")
             cook_book.get(last_key).append(dict(ingredient_name=name, quantity=int(qt), measure=msure))
 
-print(cook_book)
+# print(cook_book)
+# Задача №2
+dishes = []
+for key in cook_book:
+    dishes.append(key)
+    # print(dishes)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingridients_quantity = {}
+    for dish in dishes:
+        # print(dish)
+        if dish not in cook_book.keys():
+            print('Такого блюда нет в списке рецептов!')
+            break
+        else:
+            for el in cook_book[dish]:
+                ingr_name = el['ingredient_name']
+                measure = el['measure']
+                quantity = el['quantity'] * person_count
+                ingridients_quantity[ingr_name] = {'measure': measure, 'quantity': quantity}
+    print(ingridients_quantity)
+get_shop_list_by_dishes(dishes, 5)
